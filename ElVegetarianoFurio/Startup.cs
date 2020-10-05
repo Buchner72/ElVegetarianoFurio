cc433109059c4953121c8388808c576233861cab
Buchner72 <franz.buchner@gmx.at>
2020-10-05 15:46:03 +0200

    3. DbContext anlegen

--- ElVegetarianoFurio/Startup.cs
+++ ElVegetarianoFurio/Startup.cs
@@ -1,11 +1,14 @@
+using ElVegetarianoFurio.Models;
 using ElVegetarianoFurio.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 
 namespace ElVegetarianoFurio
 {
@@ -21,6 +24,9 @@ namespace ElVegetarianoFurio
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VegiContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("VegiContext")));
+
             services.AddControllersWithViews();
 
             services.AddScoped<IDishRepository, FileDishRepository>(); //Jedesmal wenn ein IDishRepository Anfrage, ein FileDishRepository erhalte.
