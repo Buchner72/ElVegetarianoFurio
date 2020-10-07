0e946838a4bbd6891eae568686b0912070451ce6
Buchner72 <franz.buchner@gmx.at>
2020-10-07 11:15:14 +0200

    EF Code First Migrationen ausführen

--- ElVegetarianoFurio/Models/VegiContext.cs
+++ ElVegetarianoFurio/Models/VegiContext.cs
@@ -15,8 +15,27 @@ namespace ElVegetarianoFurio.Models
 
         }  
 
-        //Das EF wir für mich zwei Tabellen anlegen, Dishes und Caregries
+        //Das EF wird zwei Tabellen anlegen, Dishes und Caregries
         public DbSet<Dish> Dishes { get; set; }
         public DbSet<Category> Categories { get; set; }
     }
 }
+
+// ---Wichtige Infos---(Genaue angaben unter Kap.3: EF Code First Migrationen ausführen)
+
+// Nugetpackade hinzufügen
+// Microsoft.EntityFrameworkCore.Design
+
+//----Info cmd micration commandos---
+//dotnet tool install --global dotnet-ef    -- Nur einmal um das EF tool zu instalieren
+//dotnet ef                                 -- EF Tool anzeigen
+
+//Datenbank anlegen
+// 1. cd C:\Users\BucFra\Documents\GIT_GitHub\demo\ElVegetarianoFurio\ElVegetarianoFurio
+// 2. dotnet ef migrations add InitialCreate
+// 3. dotnet ef database update  
+
+// Datenbank wurde jetzt angelegt
+// Öffne Server-Explorer
+//(localdb)\mssqllocaldb
+// ElVegetarianoFurioSpa
